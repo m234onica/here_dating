85500da36fc88f34029827004c8214e3cfbc1c77
joycehsu <m234onica@gmail.com>
2020-03-05 18:21:19 +0800

    adding the messenger extensions sdk

---
+++ src/static/js/extension.js
@@ -0,0 +1,15 @@
+$(document).ready(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "//connect.facebook.com/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
+
+window.extAsyncInit = function () {
+    // getUser();
+};
