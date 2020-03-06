519caf7fc94082161bf0f25e41847f0f7f6a93d1
joycehsu <m234onica@gmail.com>
2020-03-06 19:07:12 +0800

    add placeId's validation

--- src/static/js/extension.js
+++ src/static/js/extension.js
@@ -1,6 +1,10 @@
 $("input#placeId")
     .on("keyup", function () {
         var placeId = $("#placeId").val().trim();
+
+        $(".placeId_error").remove();
+        $(".invalid-feedback").remove();
+
         if (placeId.length == 4) {
             $.ajax({
                 type: "GET",
@@ -8,19 +12,25 @@ $("input#placeId")
                 dataType: "json",
                 contentType: "application/json; charset=utf-8",
                 success: function(data) {
-                    console.log(data.placeId);
+                    $(".placeId_error").remove();
                     $("#intro-submit").attr("disabled", false);
                 },
                 error: function(data) {
-                    console.log(data.status);
-                    
+                    $('#placeId').addClass('is-invalid')
+                    .after('<div class="placeId_error invalid-feedback">該店號不存在</div>');
+                    $("#intro-submit").attr("disabled", true);
+
                 }
             })
         }
+    }).on("keyup", function () {
+        $(".placeId_error").remove();
+        $('#placeId').removeClass('is-invalid').removeClass('is-valid')
     })
 
 $("#intro-submit").on("click", function (e) {
     e.preventDefault()
+
     MessengerExtensions.getContext(
         app_id,
         function success(uids) {
