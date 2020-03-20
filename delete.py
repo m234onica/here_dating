2a0e852ce13a103d319de5c6392791ed3d13ca90
joycehsu <m234onica@gmail.com>
2020-03-20 16:49:00 +0800

    bugfix about connect db

--- delete.py
+++ delete.py
@@ -1,31 +1,38 @@
 from datetime import datetime, timedelta
-import schedule
 import time
 
 from src.db import db_session
 from src.models import Pair, status_Enum
+from src.tool import message, func, text
 from config import EXPIRED_TIME, END_TIME
+
+starttime = time.time()
+
+def get_active_data():
+    active_data = Pair.query.filter(Pair.deletedAt == None)
+    return active_data
 
 
 def delete(minutes):
+    
+    active_data = get_active_data()
     expired_time = datetime.now() - timedelta(minutes=minutes)
-
+    persona_id = func.get_persona_id()
+    
     # 等待到期的資料
     if minutes == EXPIRED_TIME:
         status = 0
-        expired_data = Pair.query.filter(Pair.playerB == None).\
-            filter(Pair.deletedAt == None).\
+        expired_data = active_data.filter(Pair.playerB == None).\
             filter(Pair.createdAt <= expired_time).all()
 
     # 聊天到期的資料
     if minutes == END_TIME:
         status = 2
-        expired_data = Pair.query.filter(Pair.playerB != None).\
-            filter(Pair.deletedAt == None).\
+        expired_data = active_data.filter(Pair.playerB != None).\
             filter(Pair.startedAt <= expired_time).all()
 
     if expired_data == []:
-        print("delete expired success")
+        print("No expired data")
         return {"status_msg": "No expired data"}, 200
 
     else:
@@ -33,14 +40,16 @@ def delete(minutes):
             expire.deletedAt = datetime.now()
             expire.status = status_Enum(status)
             db_session.commit()
-        print("delete end success")
+
+            message.push_text(id=expire.playerA, persona=persona_id, text=text.wait_expired)
+            print("delete data:", expire)
+
+        print("delete success")
         return {"status_msg": "delete success."}, 200
 
-
-
-schedule.every(1).minutes.do(delete, EXPIRED_TIME)
-schedule.every(1).minutes.do(delete, END_TIME)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == '__main__':
+    while True:
+        delete(EXPIRED_TIME)
+        delete(END_TIME)
+        db_session.remove()
+        time.sleep(60.0 - ((time.time() - starttime) % 60))
