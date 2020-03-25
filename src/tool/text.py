1f0e243af14da9c2342c4c91ae8db7bea5a3b59b
joycehsu <m234onica@gmail.com>
2020-03-25 19:34:51 +0800

    change words

--- src/tool/text.py
+++ src/tool/text.py
@@ -1,11 +1,12 @@
-menu_start = "開始聊天"
-menu_rule = "使用規則"
+menu_start = "開始使用"
+menu_rule = "規則說明"
 menu_leave = "我想離開"
+menu_waiting_cancel = "取消等待"
 
-introduction = ["您好，歡迎進入 HereDating！這是一個可以與同一個地點的陌生人匿名聊天的小遊戲 ，每場聊天限時十分鐘。如果覺得聊天氣氛不錯，結束時可以選擇留下聯絡資料與對方成為朋友哦！",
-                "目前 HereDating 僅提供活動現場或特定場域使用，請點選下方按鈕開啟頁面，輸入該場活動的編號來與大家匿名聊天吧！"]
+introduction = ["您好，歡迎進入 HereDating！這是一個可以與同一個地點或活動的陌生人匿名聊天的小遊戲 ，每場聊天限時十分鐘。",
+                "目前 HereDating 僅提供特定地點使用，請點選下方按鈕開啟頁面，輸入地標編號來與大家匿名聊天吧！"]
 
-start_chating = "開始聊天"
+start_chating = "開始使用"
 
 waiting_pair = "配對中，請稍等..."
 wait_expired = "由於超過 2 分鐘沒有找到聊天對象，系統已經自動斷開，若想重新尋找聊天對象請再次按下按鈕開始聊天。"
@@ -17,7 +18,7 @@ partner_leave_message = "哎呀！對方選擇直接離開對話，要不要再�
 
 timeout_text = ["時間到囉！聊天室已關閉，您覺得剛剛聊的還愉快嗎？",
                       "點擊下面按鈕將可以選擇留下 FB 或 LINE 給對方，如果覺得聊天過程很愉快，不妨和對方交個朋友吧！"]
-send_partner_last_message_button = "留下遺言"
+send_partner_last_message_button = "留下最後一段話"
 
 user_last_message = "這是我傳的遺言："
 partner_last_message = "這是對方傳的遺言："
