e6edbe75d42f3f74b2eba6b544caefeb2aac391c
joycehsu <m234onica@gmail.com>
2020-04-10 13:04:17 +0800

    cros

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 from src.route.api import api
 from src.route.bot import bot
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.url_map.strict_slashes = False
     app.config.from_object("config")
 
