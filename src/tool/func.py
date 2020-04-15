2b1d3a7191a03504c812e98394c2a2b468209ad3
joycehsu <m234onica@gmail.com>
2020-04-15 17:15:57 +0800

    change GCS path

--- src/tool/func.py
+++ src/tool/func.py
@@ -84,7 +84,7 @@ def timeout_chat(userId):
                 text=text.timeout_text[1],
                 first_url="/message/" + userId,
                 first_title=text.send_partner_last_message_button,
-                sec_url="/pair", sec_title=text.pair_again_button)
+                sec_url="/pair.html", sec_title=text.pair_again_button)
 
             message.delete_menu(userId)
 
