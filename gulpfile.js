6c5802bcbe9a5c521ebb85ddb83f3145f04ae53f
joycehsu <m234onica@gmail.com>
2020-04-15 20:36:21 +0800

    gulp webserver

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,19 @@ gulp.task("replace", function () {
         .pipe(gulp.dest("static"));
 })
 
+gulp.task("webserver", function() {
+    setTimeout(function () {
+        gulp.src("static")                   // 預設開啟路徑
+            .pipe(webserver({                     // 啟動 webserver
+                livereload: true,                   // Livereload 的功能
+                open: false,                        // 是否自動開啟 瀏覽器
+                host: '0.0.0.0',                    // 如果使用 0.0.0.0 的 ip，還會另外開啟 wifi 等對外網路
+                port: 5001,                        // 開放通訊埠
+            }));
+    }, 1000);
+})
 
 gulp.task('default', gulp.series("clean", "compile", "revsion", "replace", function (done) {
     done();
 }));
+
