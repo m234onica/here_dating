b567e9e8203fb15f019ed3e4e06311ad6776991b
joycehsu <m234onica@gmail.com>
2020-04-17 21:50:11 +0800

    push to here_dating_914 google function

---
+++ main.py
@@ -0,0 +1,29 @@
+import werkzeug.datastructures
+from flask_cors import CORS
+
+from src import create_app
+from src.db import db_session
+
+app = create_app()
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+def main(request):
+    print(request)
+    with app.app_context():
+        headers = werkzeug.datastructures.Headers()
+        for key, value in request.headers.items():
+            headers.add(key, value)
+        with app.test_request_context(method=request.method, base_url=request.base_url, path=request.path, query_string=request.query_string, headers=headers, data=request.data):
+            try:
+                rv = app.preprocess_request()
+                if rv is None:
+                    rv = app.dispatch_request()
+            except Exception as e:
+                rv = app.handle_user_exception(e)
+            response = app.make_response(rv)
+            return app.process_response(response)
