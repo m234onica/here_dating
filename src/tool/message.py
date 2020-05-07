fbe8e13613a197672e3bc2ca9d66300171fe8e97
joycehsu <m234onica@gmail.com>
2020-05-07 19:13:58 +0800

    message.py json pull to data.json

--- src/tool/message.py
+++ src/tool/message.py
@@ -1,71 +1,40 @@
 import requests
 import os
+import json
 from urllib.parse import urljoin
 from src.tool import request
 from src.tool.text import Context
 from config import Config
+from jinja2 import Environment, PackageLoader
 
+json_file = Environment(loader=PackageLoader("src", "./static/data"))
 
 def sender_action(id, action):
-    data = {
-        "recipient": {
-            "id": id
-        },
-        "sender_action": action
-    }
+    template = json_file.get_template("data.json")
+    rendered = template.module.sender_action(id=id, action=action)
+    data = json.loads(rendered)
     return request.post("messages", data)
 
 
 def push_text(id, persona, text):
-    data = {
-        "recipient": {
-            "id": id
-        },
-        "persona_id": persona,
-        "message": {
-            "text": text
-        }
-    }
+    template = json_file.get_template("data.json")
+    rendered = template.module.push_text(id=id, persona=persona, text=text)
+    data = json.loads(rendered)
     return request.post("messages", data)
 
 
 def push_quick_reply(id, persona, text):
-    data = {
-        "recipient": {
-            "id": id
-        },
-        "messaging_type": "RESPONSE",
-        "persona_id": persona,
-        "message": {
-            "text": text,
-            "quick_replies": [
-                {
-                    "content_type": "text",
-                    "title": "Hi",
-                    "payload": "Hi",
-                }
-            ]
-        }
-    }
+    template = json_file.get_template("data.json")
+    rendered = template.module.push_quick_reply(
+        id=id, persona=persona, text=text)
+    data = json.loads(rendered)
     return request.post("messages", data)
 
 
 def push_attachment(id, persona, url):
-    data = {
-        "recipient": {
-            "id": id
-        },
-        "persona_id": persona,
-        "message": {
-            "attachment": {
-                "type": "image",
-                "payload": {
-                    "is_reusable": True,
-                    "url": url
-                }
-            }
-        }
-    }
+    template = json_file.get_template("data.json")
+    rendered = template.module.push_attachment(id=id, persona=persona, url=url)
+    data = json.loads(rendered)
     return request.post("messages", data)
 
 
@@ -120,79 +89,34 @@ def button_type(types, title, payload):
 def get_started():
     params = {"access_token": Config.PAGE_ACCESS_TOKEN}
 
-    whitelisted_domains = {
-        "whitelisted_domains": [
-            Config.BASE_URL,
-            Config.STATIC_URL
-        ]
-    }
     pair_url = urljoin(Config.STATIC_URL, "pair.html")
     rule_url = urljoin(Config.STATIC_URL, "rule.html")
-    data = {
-        "get_started": {
-            "payload": "Start"
-        },
-        "greeting": [
-            {
-                "locale": "default",
-                "text": "我是問候語\n我是問候語我是問候語我是問候語\n我是問候語\n"
-            }
-        ],
-        "persistent_menu": [
-            {
-                "locale": "default",
-                "composer_input_disabled": False,
-                "call_to_actions": [
-                    {
-                        "type": "web_url",
-                        "title": Context.menu_start,
-                        "url": pair_url,
-                        "messenger_extensions": True,
-                        "webview_height_ratio": "full"
-                    },
-                    {
-                        "type": "web_url",
-                        "title": Context.menu_rule,
-                        "url": rule_url,
-                        "messenger_extensions": True,
-                        "webview_height_ratio": "full"
-                    }
-                ]
-            }
-        ]
-    }
+
+    template = json_file.get_template("data.json")
+    rendered = template.module.get_started(
+        menu_start=Context.menu_start, pair_url=pair_url,
+        menu_rule=Context.menu_rule, rule_url=rule_url)
+
+    data = json.loads(rendered)
     get_start_responese = request.post("messenger_profile", data)
+
+    whitelisted_domains = {
+        "whitelisted_domains": [Config.BASE_URL, Config.STATIC_URL]}
     whitelisted_domains_response = request.post(
         "messenger_profile", whitelisted_domains)
+
     response = [get_start_responese, whitelisted_domains_response]
     return response
 
 
 def push_pairing_menu(id):
     url = urljoin(Config.STATIC_URL, "rule.html")
-    data = {
-        "psid": id,
-        "persistent_menu": [
-            {
-                "locale": "default",
-                "composer_input_disabled": False,
-                "call_to_actions": [
-                    {
-                        "type": "postback",
-                        "title": Context.menu_waiting_cancel,
-                        "payload": "Leave"
-                    },
-                    {
-                        "type": "web_url",
-                        "title": Context.menu_rule,
-                        "url": url,
-                        "messenger_extensions": True,
-                        "webview_height_ratio": "full"
-                    }
-                ]
-            }
-        ]
-    }
+
+    template = json_file.get_template("data.json")
+    rendered = template.module.custom_menu(id=id,
+                                           postback_title=Context.menu_waiting_cancel,
+                                           url_title=Context.menu_rule, url=url)
+    data = json.loads(rendered)
     response = request.post("custom_user_settings", data)
     print("pairing_menu:", response)
     return response
@@ -200,29 +124,12 @@ def push_pairing_menu(id):
 
 def push_paired_menu(id):
     url = urljoin(Config.STATIC_URL, "rule.html")
-    data = {
-        "psid": id,
-        "persistent_menu": [
-            {
-                "locale": "default",
-                "composer_input_disabled": False,
-                "call_to_actions": [
-                    {
-                        "type": "postback",
-                        "title": Context.menu_leave,
-                        "payload": "Leave"
-                    },
-                    {
-                        "type": "web_url",
-                        "title": Context.menu_rule,
-                        "url": url,
-                        "messenger_extensions": True,
-                        "webview_height_ratio": "full"
-                    }
-                ]
-            }
-        ]
-    }
+
+    template = json_file.get_template("data.json")
+    rendered = template.module.custom_menu(id=id,
+                                           postback_title=Context.menu_leave,
+                                           url_title=Context.menu_rule, url=url)
+    data = json.loads(rendered)
     response = request.post("custom_user_settings", data)
     print("paired_menu: ", response)
     return response
