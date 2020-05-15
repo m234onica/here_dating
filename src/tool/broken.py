920971b6045bd70f7ae9cb03f3e54256eb1670a0
joycehsu <m234onica@gmail.com>
2020-05-15 19:34:28 +0800

    commit db before sending timeout message

--- src/tool/broken.py
+++ src/tool/broken.py
@@ -12,11 +12,15 @@ def timeout(userId):
     pair = filter.get_active_pair(userId)
     recipient_id = filter.get_recipient_id(userId)
     now_time = datetime.now()
+    pair.deletedAt = now_time
+
     if pair.startedAt == None and pair.playerB == None:
         if now_time - timedelta(minutes=Config.EXPIRED_TIME) < pair.createdAt:
             return response(msg="User is pairing", payload={"status": "pairing"}, code=200)
         else:
             pair.status = status_Enum(0)
+            db_session.commit()
+
             message.delete_menu(userId)
             reply.quick_pair(userId, pair.placeId, Context.wait_expired)
     else:
@@ -24,11 +28,10 @@ def timeout(userId):
             return response(msg="User is paired", payload={"status": "paired"}, code=200)
         else:
             pair.status = status_Enum(2)
+            db_session.commit()
+
             reply.timeout_message(userId)
             reply.timeout_message(recipient_id)
-
-    pair.deletedAt = now_time
-    db_session.commit()
 
     return response(msg="Timeout to breaked pair", payload={"status": "pair_broken"}, code=200)
 
