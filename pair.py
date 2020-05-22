c5f44fdfd0560f33b6ec1c56a1cc4b16cbb0505e
joycehsu <m234onica@gmail.com>
2020-05-22 16:24:55 +0800

    Group userId by placeId. concat column userId if data has same placeId

--- pair.py
+++ pair.py
@@ -2,7 +2,7 @@ import asyncio
 import aiomysql
 
 from config import Config
-
+from src.tool import reply
 loop = asyncio.get_event_loop()
 
 
@@ -10,5 +10,20 @@ async def pool():
 
     conn = await aiomysql.connect(host=Config.HOST, port=Config.PORT, user=Config.USER, password=Config.PASSWORD, db=Config.NAME, loop=loop)
 
+    async with conn.cursor() as cur:
+
+        pool = ''' SELECT 
+                        placeId, 
+                        GROUP_CONCAT(userId SEPARATOR ",") as userId
+                    FROM
+                        pool 
+                    WHERE 
+                        deletedAt is NULL 
+                    GROUP BY 
+                        placeId; '''
+        await cur.execute(pool)
+        group = await cur.fetchall()
+
+    conn.close()
 
 loop.run_until_complete(pool())
