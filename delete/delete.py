2d88bd24b9626f46534fc8f1056c2ff10b96e628
joycehsu <m234onica@gmail.com>
2020-06-01 15:09:09 +0800

    delete pair code in google cloud functions

---
+++ delete/delete.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+import base64
+import time
+
+from src.db import db_session
+from src.models import Pair, Pool, status_Enum
+from src import message, filter, reply
+from src.func import expired_time
+from src.context import Context
+from config import Config
+
+starttime = time.time()
+
+
+def send_expired_message(pool):
+    userId = pool.userId
+    placeId = pool.placeId
+
+    message.delete_menu(userId)
+    return reply.quick_pair(userId, placeId, Context.wait_expired)
+
+
+def delete(minutes):
+    time_diff = expired_time(minutes)
+
+    if minutes == Config.EXPIRED_TIME:
+        expired_pool = filter.get_expired_pool(time_diff)
+
+        if expired_pool == []:
+            print("No expired pairing")
+            return {"status_msg": "No expired pairing"}, 200
+
+        for pool in expired_pool:
+            pool.deletedAt = datetime.now()
+            send_expired_message(pool)
+            print("delete pairing:", pool)
+
+    if minutes == Config.END_TIME:
+        expired_data = filter.get_expired_pair(time_diff)
+
+        if expired_data == []:
+            print("No expired paired")
+            return {"status_msg": "No expired paired"}, 200
+
+        for data in expired_data:
+            data.deletedAt = datetime.now()
+            data.status = status_Enum(2)
+
+            reply.timeout_message(data.playerA)
+            reply.timeout_message(data.playerB)
+
+            print("delete paired:", data)
+
+    db_session.commit()
+    print("delete success")
+    return {"status_msg": "delete success."}, 200
---
+++ delete/src/message.py
@@ -0,0 +1,111 @@
+import requests
+import os
+import json
+from urllib.parse import urljoin, urlencode
+from jinja2 import Environment, PackageLoader
+
+from src.func import api_request
+from src.context import Context
+from config import Config
+
+json_file = Environment(loader=PackageLoader("src", "static/data"))
+
+
+def sender_action(id, action):
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.sender_action(id=id, action=action)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","messages"], json=data)
+
+
+def push_text(id, persona, text):
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.push_text(id=id, persona=persona, text=text)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","messages"], json=data)
+
+
+def push_quick_reply(id, persona, text):
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.push_quick_reply(
+        id=id, persona=persona, text=text)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","messages"], json=data)
+
+
+def push_attachment(id, persona, url):
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.push_attachment(id=id, persona=persona, url=url)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","messages"], json=data)
+
+
+def push_button(id, persona, text, types, title, payload):
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.push_button(
+        id=id, persona=persona, text=text, types=types, payload=payload, title=title)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","messages"], json=data)
+
+
+def get_started():
+    params = {"access_token": Config.PAGE_ACCESS_TOKEN}
+
+    pair_url = urljoin(Config.STATIC_URL, "pair.html")
+    rule_url = urljoin(Config.STATIC_URL, "rule.html")
+
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.get_started(
+        username="{{user_first_name}}",
+        menu_start=Context.menu_start, pair_url=pair_url,
+        menu_rule=Context.menu_rule, rule_url=rule_url)
+
+    data = json.loads(rendered)
+    get_start_responese = api_request(
+        "POST", urls=["me","messenger_profile"], json=data)
+
+    whitelisted_domains = {
+        "whitelisted_domains": [Config.BASE_URL, Config.STATIC_URL]}
+    whitelisted_domains_response = api_request(
+        "POST", urls=["me","messenger_profile"], json=whitelisted_domains)
+
+    response = [get_start_responese, whitelisted_domains_response]
+    return response
+
+
+def push_customer_menu(id, postback_title):
+    url = urljoin(Config.STATIC_URL, "rule.html")
+
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.custom_menu(id=id,
+                                           postback_title=postback_title,
+                                           url_title=Context.menu_rule, url=url)
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","custom_user_settings"], json=data)
+
+
+def delete_menu(id):
+    params = {
+        "psid": id,
+        "params": '["persistent_menu"]',
+        "access_token": Config.PAGE_ACCESS_TOKEN
+    }
+    return api_request("DELETE", urls=["me","custom_user_settings"], params=params)
+
+
+def persona():
+    template = json_file.get_template("data.json.jinja")
+    rendered = template.module.persona()
+    data = json.loads(rendered)
+    return api_request("POST", urls=["me","personas"], json=data)
+
+
+def get_username(userId):
+    params = urlencode(
+        {"fields": "first_name, last_name", "access_token": Config.PAGE_ACCESS_TOKEN})
+
+    user_profile = api_request(
+        "GET", urls=[userId], params=params)
+
+    username = user_profile["last_name"] + user_profile["first_name"]
+    return username
