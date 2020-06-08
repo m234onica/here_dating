bcec3f85d81b17574c007192581110621e520a67
joycehsu <m234onica@gmail.com>
2020-06-08 15:38:42 +0800

    mv message api

--- pairing_pool/pair.py
+++ pairing_pool/pair.py
@@ -1,66 +1,12 @@
 import ssl
 import certifi
-import json
 import asyncio
 import aiomysql
 import aiohttp
-import requests
 from urllib.parse import urljoin
-from jinja2 import Environment, PackageLoader
 
 from config import mysql, Config
-from src.context import Context
-
-json_file = Environment(loader=PackageLoader("src", "static/data"))
-
-def get_persona_id():
-    url = urljoin(Config.FB_API_URL, "/me/personas")
-    params = {"access_token": Config.PAGE_ACCESS_TOKEN}
-    persona = requests.get(url=url, params=params).json()
-    return persona["data"][0]["id"]
-
-
-async def push_text(session, id):
-    url = urljoin(Config.FB_API_URL, "/me/messages")
-    params = {"access_token": Config.PAGE_ACCESS_TOKEN}
-    persona_id = get_persona_id()
-
-    template = json_file.get_template("data.json.jinja")
-    rendered = template.module.push_text(
-        id=id, persona=persona_id, text=Context.waiting_success[0])
-    data = json.loads(rendered)
-
-    async with session.post(url, params=params, json=data) as response:
-        return await response.text()
-
-
-async def push_quick_reply(session, id):
-    url = urljoin(Config.FB_API_URL, "/me/messages")
-    params = {"access_token": Config.PAGE_ACCESS_TOKEN}
-    persona_id = get_persona_id()
-
-    template = json_file.get_template("data.json.jinja")
-    rendered = template.module.push_quick_reply(
-        id=id, persona=persona_id, text=Context.waiting_success[1])
-    data = json.loads(rendered)
-
-    async with session.post(url, params=params, json=data) as response:
-        return await response.text()
-
-
-async def push_customer_menu(session, id):
-    url = urljoin(Config.FB_API_URL, "/me/custom_user_settings")
-    params = {"access_token": Config.PAGE_ACCESS_TOKEN}
-    static_url = urljoin(Config.STATIC_URL, "rule.html")
-
-    template = json_file.get_template("data.json.jinja")
-    rendered = template.module.custom_menu(id=id,
-                                           postback_title=Context.menu_leave,
-                                           url_title=Context.menu_rule, url=static_url)
-    data = json.loads(rendered)
-
-    async with session.post(url, params=params, json=data) as response:
-        return await response.text()
+from src import message
 
 
 async def pair(conn):
