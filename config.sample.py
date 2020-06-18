b63aebff41a5a4c6a4dbb585e4bfdad32a8034d3
joycehsu <m234onica@gmail.com>
2020-06-18 15:26:44 +0800

    add url trailing slash

--- config.sample.py
+++ config.sample.py
@@ -9,21 +9,25 @@ mysql = {
 }
 
 class Config:
+    def url_trailing_slash(url: str):
+        if not url.endswith("/"):
+            return url + "/"
+        return url
+
     # delete timing
     EXPIRED_TIME = 2
     END_TIME = 3
 
     DEBUG = True
-    SECRET_KEY = "="
+    SECRET_KEY = ""
     FLASK_APP = os.environ.get("FLASK_APP")
     FLASK_ENV = os.environ.get("FLASK_ENV")
 
     # here_dating messenger settings
-    APP_ID = ""
     PAGE_VERIFY_TOKEN = ""
     PAGE_ACCESS_TOKEN = ""
-    BASE_URL = ""
-    STATIC_URL = ""
+    BASE_URL = url_trailing_slash("")
+    STATIC_URL = url_trailing_slash("")
 
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{NAME}?charset=utf8mb4".format(
         USER=mysql["username"],
