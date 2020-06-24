c7065502bfab121e1d018d5f89f7d2bf33d57f16
joycehsu <m234onica@gmail.com>
2020-06-24 16:10:11 +0800

    fix pair main function

--- async_func/run.py
+++ async_func/run.py
@@ -2,10 +2,10 @@ import asyncio
 from pair import pair
 from delete import delete
 
-def main():
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     future = [
         asyncio.ensure_future(pair.main(loop)),
         asyncio.ensure_future(delete.main(loop))
-        ]
+    ]
     loop.run_until_complete(asyncio.gather(*future))
