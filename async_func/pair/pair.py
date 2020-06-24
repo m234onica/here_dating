72b891f8a13c87b05a0849e41e1963d1b0572746
joycehsu <m234onica@gmail.com>
2020-06-24 16:08:18 +0800

    mv func to select.py

--- async_func/pair/pair.py
+++ async_func/pair/pair.py
@@ -8,28 +8,11 @@ import warnings
 from urllib.parse import urljoin
 from datetime import datetime, timedelta
 from config import mysql, Config
-from pair import message
+from pair import message, select
 
 warnings.filterwarnings("ignore", category=aiomysql.Warning)
 logging.basicConfig(
     format='%(asctime)s %(levelname)s - %(message)s', level=logging.INFO)
-
-
-async def select(loop, pool):
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            group = []
-            get_pool_data = ''' SELECT
-                            placeId,
-                            GROUP_CONCAT(userId ORDER BY rand() SEPARATOR ",") as userId
-                        FROM
-                            pool
-                        WHERE
-                            deletedAt is NULL AND createdAt > CURRENT_TIME() - INTERVAL {} MINUTE
-                        GROUP BY
-                            placeId; '''
-            await cur.execute(get_pool_data.format(Config.EXPIRED_TIME))
-            return await cur.fetchall()
 
 
 async def insert(loop, sql, data, pool):
@@ -57,51 +40,20 @@ async def unpair_count(loop, pool):
                     status = 1
                     logging.warning(
                         "PlaceId: {} was cut by GROUP_CONCAT()".format(count[0]))
-    return status
-
-
-async def filter_accept_pairing(loop, pool, userId, user_list):
-    accept_pairing_list = []
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            sql = '''SELECT playerB FROM pair WHERE playerA="{userId}" 
-                    UNION ALL SELECT playerA FROM pair WHERE playerB="{userId}";'''
-            await cur.execute(sql.format(userId=userId))
-            has_paired_list = await cur.fetchall()
-
-            accept_pairing_list = list.copy(user_list)
-            for userId in has_paired_list:
-                if userId[0] in accept_pairing_list:
-                    accept_pairing_list.remove(userId[0])
-            return accept_pairing_list[-1]
-
-
-async def reset_pairing(loop, pool, placeId):
-    reset_list = []
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            sql = ''' SELECT userId
-                    FROM (SELECT userId, MAX(deletedAt) AS deletedAt FROM pool WHERE status=1 and placeId={placeId} GROUP BY userId) AS last_pool
-                    WHERE deletedAt <= DATE_SUB(current_time(), INTERVAL {time} MINUTE) 
-                    ORDER BY RAND();'''
-            await cur.execute(sql.format(placeId=placeId, time=Config.RESET_PAIRING_TIME))
-            data = await cur.fetchall()
-            for d in data:
-                reset_list.append(d[0])
-    return reset_list
+            return status
 
 
 async def pair(loop, pool):
-    user_list = []
+    message_list = []
     data = []
 
-    group = await select(loop, pool)
+    group = await select.all_pairing(loop, pool)
     for i in range(len(group)):
         placeId = group[i][0]
         pairing_list = group[i][1].split(",")
         while len(pairing_list) > 1:
             playerA = pairing_list[0]
-            playerB = await filter_accept_pairing(loop, pool, playerA, pairing_list)
+            playerB = await select.filter_accept_pairing(loop, pool, playerA, pairing_list)
             if playerB != playerA:
                 message_list.append(playerA)
                 message_list.append(playerB)
@@ -110,7 +62,7 @@ async def pair(loop, pool):
                 pairing_list.remove(playerB)
             pairing_list.remove(playerA)
 
-        reset_list = await reset_pairing(loop, pool, placeId)
+        reset_list = await select.reset_pairing(loop, pool, placeId)
         while len(reset_list) > 1:
             playerA, playerB = reset_list[:2]
 
---
+++ async_func/pair/select.py
@@ -0,0 +1,52 @@
+import asyncio
+import aiomysql
+from urllib.parse import urljoin
+from config import Config
+
+
+async def all_pairing(loop, pool):
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            group = []
+            get_pool_data = ''' SELECT
+                            placeId,
+                            GROUP_CONCAT(userId ORDER BY rand() SEPARATOR ",") as userId
+                        FROM
+                            pool
+                        WHERE
+                            deletedAt is NULL AND createdAt > CURRENT_TIME() - INTERVAL {} MINUTE
+                        GROUP BY
+                            placeId; '''
+            await cur.execute(get_pool_data.format(Config.EXPIRED_TIME))
+            return await cur.fetchall()
+
+
+async def filter_accept_pairing(loop, pool, userId, user_list):
+    accept_pairing_list = []
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            sql = '''SELECT playerB FROM pair WHERE playerA="{userId}" 
+                    UNION ALL SELECT playerA FROM pair WHERE playerB="{userId}";'''
+            await cur.execute(sql.format(userId=userId))
+            has_paired_list = await cur.fetchall()
+
+            accept_pairing_list = list.copy(user_list)
+            for userId in has_paired_list:
+                if userId[0] in accept_pairing_list:
+                    accept_pairing_list.remove(userId[0])
+            return accept_pairing_list[-1]
+
+
+async def reset_pairing(loop, pool, placeId):
+    reset_list = []
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            sql = ''' SELECT userId
+                    FROM (SELECT userId, MAX(deletedAt) AS deletedAt FROM pool WHERE status=1 and placeId={placeId} GROUP BY userId) AS last_pool
+                    WHERE deletedAt <= DATE_SUB(current_time(), INTERVAL {time} MINUTE) 
+                    ORDER BY RAND();'''
+            await cur.execute(sql.format(placeId=placeId, time=Config.RESET_PAIRING_TIME))
+            data = await cur.fetchall()
+            for d in data:
+                reset_list.append(d[0])
+    return reset_list
